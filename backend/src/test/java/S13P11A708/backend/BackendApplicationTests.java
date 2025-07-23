57e0ba3d285dc2a9f5133a4710a8078639e13478
DESKTOP-DLOPF19\SSAFY <taegyunb99@gmail.com>
2025-07-23 11:03:46 +0900

    initial commit

---
+++ backend/src/test/java/S13P11A708/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package S13P11A708.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
