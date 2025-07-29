bc8f354ea2326bcff25e69c8ea5a6514aa79f502
조현아 <cindy0810.cho@gmail.com>
2025-07-29 17:43:07 +0900

    [Fix] router import 안불러온 것 수정

--- frontend/connect-pjt/src/router/index.js
+++ frontend/connect-pjt/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MissionView from '../views/MissionView.vue'
 import Onboarding from '@/views/onboarding.vue'
+import MissionCreateView from '@/views/MissionCreateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
