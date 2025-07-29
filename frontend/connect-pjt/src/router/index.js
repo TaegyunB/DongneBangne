db2f5333707afbd018b900ca59c04894717cca16
조현아 <cindy0810.cho@gmail.com>
2025-07-29 17:30:39 +0900

    [Feat] 도전 미션 페이지 생성 완료 #22

--- frontend/connect-pjt/src/router/index.js
+++ frontend/connect-pjt/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MissionView from '../views/MissionView.vue'
+import Onboarding from '@/views/onboarding.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,16 @@ const router = createRouter({
       name: 'mission',
       component: MissionView,
     },
+    {
+      path: '/login',
+      name: 'onboarding',
+      component: Onboarding
+    },
+    {
+      path: '/admin/challenges',
+      name: 'missionCreate',
+      component: MissionCreateView,
+    }
   ],
 
   
