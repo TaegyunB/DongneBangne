56cbf3a59a8fbe48ff0c2f4418e6606ebde3807a
심양관 <cash3543@naver.com>
2025-07-30 15:21:53 +0900

    [Feat] 로그인 위아래 버튼 및 카카오톡 로그인 진입성공 #30

--- frontend/connect-pjt/src/router/index.js
+++ frontend/connect-pjt/src/router/index.js
@@ -1,26 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MissionView from '../views/MissionView.vue'
-import Onboarding from '@/views/onboarding.vue'
+import Onboarding from '../views/onboarding.vue'
 import MissionCreateView from '@/views/MissionCreateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/challenges',
-      name: 'mission',
-      component: MissionView,
-    },
-    {
       path: '/login',
       name: 'onboarding',
       component: Onboarding
     },
     {
+
+    },
+    {
+      path: '/challenges',
+      name: 'mission',
+      component: MissionView,
+    },
+    {
       path: '/admin/challenges',
       name: 'missionCreate',
       component: MissionCreateView,
-    }
+    },
   ],
 
   
