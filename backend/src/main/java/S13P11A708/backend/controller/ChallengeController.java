193f5cbf0c25212fa33b3faebafb68129df39915
DESKTOP-DLOPF19\SSAFY <taegyunb99@gmail.com>
2025-07-30 13:44:22 +0900

    [Test] 백엔드 브랜치 테스트

--- backend/src/main/java/S13P11A708/backend/controller/ChallengeController.java
+++ backend/src/main/java/S13P11A708/backend/controller/ChallengeController.java
@@ -1,9 +1,11 @@
 package S13P11A708.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/challenges")
 public class ChallengeController {
 }
