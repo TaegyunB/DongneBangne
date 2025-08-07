707edf2cdb92a7485a89c2a7688e531d38baa992
심양관 <cash3543@naver.com>
2025-08-07 10:12:17 +0900

    [Feat] Mainpage Url ranking -> rankings #79

--- backend/src/main/java/S13P11A708/backend/websocket/GameMessageFactory.java
+++ backend/src/main/java/S13P11A708/backend/websocket/GameMessageFactory.java
@@ -1,7 +1,10 @@
 package S13P11A708.backend.websocket;
 
+import org.springframework.context.annotation.Configuration;
+
 import S13P11A708.backend.domain.enums.GameMessageType;
 
+@Configuration
 public class GameMessageFactory {
 
     public static GameSocketMessage createMessage(GameMessageType type, Long roomId, String payload) {
