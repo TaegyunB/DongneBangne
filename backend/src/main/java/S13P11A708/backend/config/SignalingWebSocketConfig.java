a037b7815698a6504f09ad9110b2b0a0b7413754
XsQuare01 <mystic6113@naver.com>
2025-08-08 16:59:42 +0900

    [Feat] Raw WebSocket과 STOMP 분리

--- backend/src/main/java/S13P11A708/backend/config/SignalingWebSocketConfig.java
+++ backend/src/main/java/S13P11A708/backend/config/SignalingWebSocketConfig.java
@@ -1,6 +1,7 @@
 
-package S13P11A708.backend.websocket;
+package S13P11A708.backend.config;
 
+import S13P11A708.backend.websocket.SignalingHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -9,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+public class SignalingWebSocketConfig implements WebSocketConfigurer {
     // signal로 요청이 들어오면 signalingHandler가 동작한다.
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
--- backend/src/main/java/S13P11A708/backend/config/WebSocketConfig.java
+++ backend/src/main/java/S13P11A708/backend/config/WebSocketConfig.java
@@ -1,10 +1,10 @@
 package S13P11A708.backend.config;
 
+import S13P11A708.backend.websocket.SignalingHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -23,9 +23,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/sub", "/queue");
         // send destination(클라이언트 -> 서버)
         registry.setApplicationDestinationPrefixes("/pub");
-
-
     }
-
-
 }
