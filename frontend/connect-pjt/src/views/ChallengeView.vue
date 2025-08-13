3f9abdb0882e3d8ca7305b8003a7700d6fc70933
조현아 <cindy0810.cho@gmail.com>
2025-08-13 16:22:36 +0900

    [Feat] 도전 목록 페이지 수정 #130

--- frontend/connect-pjt/src/views/ChallengeView.vue
+++ frontend/connect-pjt/src/views/ChallengeView.vue
@@ -200,7 +200,7 @@
         </div>
       </div>
     </div>
-<!-- . -->
+<!-- .. -->
     <!-- 최종 삭제 확인 모달 -->
     <div v-if="showFinalDeleteModal" class="modal-overlay" @click.self="closeFinalDeleteModal">
       <div class="modal-content delete-modal">
