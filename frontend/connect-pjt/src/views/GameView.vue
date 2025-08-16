29c568d31bb604cbc684b4aea158ac64a83c7e31
XsQuare01 <mystic6113@naver.com>
2025-08-17 00:25:13 +0900

    [Fix] stomp 연결 수정

--- frontend/connect-pjt/src/views/GameView.vue
+++ frontend/connect-pjt/src/views/GameView.vue
@@ -103,7 +103,7 @@ export default {
 
             this.getUsersInfo(roomId)
           },
-                      'start-game': () => {
+          'start-game': () => {
               console.log('Unity → Vue 게임 시작 요청:')
               const jsonData = this.parseUnityData(data.data)
               const { roomId, userId } = jsonData
@@ -788,12 +788,13 @@ export default {
     },
 
     // STOMP 토픽 구독
-    subscribeToTopics() {
+    subscribeToTopics(roomId, userId) {
       if (!this.stompClient || !this.stompClient.connected) {
         console.warn('STOMP 클라이언트가 연결되지 않았습니다.')
         return
       }
 
+      console.log(`roomId: ${this.roomId}, userId: ${this.userId}`)
       try {
         // 1. 기본 구독 경로 (/sub)
         this.stompClient.subscribe('/sub', (message) => {
